9a38e93e92b2bcdf9a7444c8c2288e3d8a3c5e06
mura-lara <murayamawebpro@gmail.com>
2023-09-15 14:05:31 +0900

    Libs 共通関数学習 DI:コンストラクタ

--- src/app/Http/Controllers/UtilController.php
+++ src/app/Http/Controllers/UtilController.php
@@ -7,15 +7,28 @@ use App\Libs\Util;
 
 class UtilController extends Controller
 {
-    public function index(Util $util)
-    {
+    protected $util;
 
+    public function __construct(Util $util)
+    {
+        $this->util = $util;
+    }
+
+    public function index()
+    {
         // $result = $util->isInputValid(null); // false
-        $result = $util->isInputValid(''); // false
-        $result = $util->isInputValid([]); // false
-        $result = $util->isInputValid(0); // true
-        $result = $util->isInputValid('テキスト'); // true
-        $result = $util->isInputValid(['a', 'b', 'c']); // true
+        // $result = $util->isInputValid(''); // false
+        // $result = $util->isInputValid([]); // false
+        // $result = $util->isInputValid(0); // true
+        // $result = $util->isInputValid('テキスト'); // true
+        // $result = $util->isInputValid(['a', 'b', 'c']); // true
+        
+        $result = $this->util->isInputValid(null); // false
+        $result = $this->util->isInputValid(''); // false
+        $result = $this->util->isInputValid([]); // false
+        $result = $this->util->isInputValid(0); // true
+        $result = $this->util->isInputValid('テキスト'); // true
+        $result = $this->util->isInputValid(['a', 'b', 'c']); // true
 
         dd($result);
 
